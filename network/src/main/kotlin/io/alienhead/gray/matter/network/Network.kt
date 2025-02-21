83059bb987e17536ef52847a94a38fde04e17946
cjpoulsen <53880342+cjpoulsen@users.noreply.github.com>
2025-02-20 19:04:13 -0600

    Refactored donor network code

--- network/src/main/kotlin/io/alienhead/gray/matter/network/Network.kt
+++ network/src/main/kotlin/io/alienhead/gray/matter/network/Network.kt
@@ -1,13 +1,18 @@
 package io.alienhead.gray.matter.network
 
 import io.alienhead.gray.matter.blockchain.Block
+import io.alienhead.gray.matter.blockchain.Blockchain
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
+import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
 
@@ -95,11 +100,31 @@ data class Network(
             client.broadcastBlock(it.address, newBlock)
         }
     }
+
+    suspend fun downloadPeers(donorNode: String) {
+        peers.addAll(client.downloadPeers(donorNode))
+    }
+
+    suspend fun downloadPeerInfo(peer: String) {
+        val peerInfo = client.downloadPeerInfo(peer)
+
+        peers.add(peerInfo.node.toNode())
+    }
+
+    suspend fun downloadBlockchain(peer: String) = client.downloadBlockchain(peer)
+
+    suspend fun updatePeer(peer: String, node: Node) {
+        client.updatePeer(peer, node)
+    }
 }
 
 interface NetworkClient {
     suspend fun broadcastPeer(address: String, node: Node)
     suspend fun broadcastBlock(address: String, block: Block)
+    suspend fun downloadPeers(address: String): List<Node>
+    suspend fun downloadPeerInfo(address: String): Info
+    suspend fun downloadBlockchain(address: String): Blockchain
+    suspend fun updatePeer(address: String, node: Node)
 }
 
 class NetworkWebClient: NetworkClient {
@@ -121,5 +146,60 @@ class NetworkWebClient: NetworkClient {
             contentType(ContentType.Application.Json)
             setBody(block)
         }
+    }
+
+    override suspend fun downloadPeers(address: String): List<Node> {
+        val response = runBlocking  { client.get("$address/network") }
+
+        if (response.status != HttpStatusCode.OK) {
+            throw RuntimeException("Failed to download peers from address: $address")
+        }
+
+        return response.body<Network>().peers()
+    }
+
+    override suspend fun downloadPeerInfo(address: String): Info {
+        val response = runBlocking { client.get("$address/info") }
+        if (response.status != HttpStatusCode.OK) {
+            throw RuntimeException("Failed to download peer info with address: $address")
+        }
+
+        return response.body<Info>()
+
+    }
+
+    override suspend fun downloadBlockchain(address: String): Blockchain {
+        // Download the blockchain
+        var page = 0
+
+        var newBlocks = downloadBlockchain(address, page).toMutableList()
+
+        val blocks = newBlocks.toMutableList()
+        while (newBlocks.isNotEmpty()) {
+            page++
+            newBlocks = downloadBlockchain(address, page).toMutableList()
+            blocks.addAll(newBlocks)
+        }
+
+        return Blockchain(chain = blocks.toMutableList())
+    }
+
+    override suspend fun updatePeer(address: String, node: Node) {
+        val response = runBlocking { client.post("$address/network/node?broadcast=true") {
+            contentType(ContentType.Application.Json)
+            setBody(node)
+        } }
+        if (response.status != HttpStatusCode.OK) {
+            // TODO since we have a list of nodes, go down the list and keep trying other nodes
+            throw RuntimeException("Failed to submit self to the network to address: $address")
+        }
+    }
+
+    private suspend fun downloadBlockchain(address: String, page: Int): List<Block> {
+        val response = client.get("$address/blockchain?page=$page&size=10")
+        if (response.status != HttpStatusCode.OK) {
+            throw RuntimeException("Failed to download the blockchain from address: $address")
+        }
+        return response.body<List<Block>>()
     }
 }
