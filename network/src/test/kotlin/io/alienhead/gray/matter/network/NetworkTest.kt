d87c42a7ef25f339461f282522860ba889b12804
cjpoulsen <53880342+cjpoulsen@users.noreply.github.com>
2025-02-24 15:41:41 -0600

    Update NetworkTest.kt

--- network/src/test/kotlin/io/alienhead/gray/matter/network/NetworkTest.kt
+++ network/src/test/kotlin/io/alienhead/gray/matter/network/NetworkTest.kt
@@ -1,11 +1,15 @@
 package io.alienhead.gray.matter.network
 
 import io.alienhead.gray.matter.blockchain.Block
+import io.alienhead.gray.matter.storage.Storage
+import io.alienhead.gray.matter.storage.StoreNode
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.collections.shouldHaveSize
 import io.mockk.coEvery
 import io.mockk.coVerify
+import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import java.time.Instant
 
 class NetworkTest: DescribeSpec({
@@ -13,44 +17,61 @@ class NetworkTest: DescribeSpec({
         describe("addPeer") {
             it("should add node to list of peers") {
                 val networkClient = mockk<NetworkClient>()
-                val network = Network(networkClient, mutableListOf())
+                val storage = mockk<Storage>()
+                val network = Network(networkClient, storage)
+
+                every { storage.addNetworkPeer(any()) } returns Unit
+                every { storage.getNetworkPeer(any()) } returns null
 
                 network.addPeer(Node("test", NodeType.REPLICA), false)
 
-                network.peers() shouldHaveSize 1
+                coVerify(exactly = 1) { storage.getNetworkPeer(any()) }
+                coVerify(exactly = 1) { storage.addNetworkPeer(any()) }
             }
 
             it("should not add duplicate peer") {
                 val networkClient = mockk<NetworkClient>()
-                val network = Network(networkClient, mutableListOf())
+                val storage = mockk<Storage>()
+                val network = Network(networkClient, storage)
+
+                every { storage.addNetworkPeer(any()) } returns Unit
+                every { storage.getNetworkPeer(any()) } returns StoreNode("test", NodeType.REPLICA.name)
 
                 network.addPeer(Node("test", NodeType.REPLICA), false)
                 network.addPeer(Node("test", NodeType.REPLICA), false)
 
-                network.peers() shouldHaveSize 1
+                coVerify(exactly = 2) { storage.getNetworkPeer(any()) }
+                coVerify(exactly = 0) { storage.addNetworkPeer(any()) }
             }
 
             it("should broadcast new peer") {
                 val networkClient = mockk<NetworkClient>()
-                val network = Network(networkClient, mutableListOf())
+                val storage = mockk<Storage>()
+                val network = Network(networkClient, storage)
 
                 coEvery { networkClient.broadcastPeer(any(), any()) } returns Unit
+                every { storage.addNetworkPeer(any()) } returns Unit
+                every { storage.getNetworkPeer(any()) } returns null
+                every { storage.peers() } returns listOf(StoreNode("test2", NodeType.REPLICA.name))
 
                 network.addPeer(Node("first", NodeType.REPLICA), false)
                 network.addPeer(Node("test", NodeType.REPLICA), true)
 
-                network.peers() shouldHaveSize 2
-
                 coVerify(exactly = 1) { networkClient.broadcastPeer(any(), any()) }
+                coVerify(exactly = 2) { storage.getNetworkPeer(any()) }
             }
         }
 
         describe("broadcastBlock") {
             it("should broadcast block to every peer") {
                 val networkClient = mockk<NetworkClient>()
-                val network = Network(networkClient, mutableListOf())
+                val storage = mockk<Storage>()
+                val network = Network(networkClient, storage)
 
                 coEvery { networkClient.broadcastBlock(any(), any()) } returns Unit
+                every { storage.getNetworkPeer(any()) } returns null
+                every { storage.addNetworkPeer(any()) } returns Unit
+                every { storage.peers() } returns listOf(StoreNode("first", NodeType.REPLICA.name), StoreNode("test", NodeType.REPLICA.name))
 
                 network.addPeer(Node("first", NodeType.REPLICA), false)
                 network.addPeer(Node("second", NodeType.REPLICA), false)
